f9e1d4d16983e7fe53e19ccca2fb427e078d551f
Avaneesh Shetti <shettiavaneesh@gmail.com>
2025-04-27 21:23:03 -0700

    set up

---
+++ src/scripts/server.js
@@ -0,0 +1,95 @@
+import pkg from 'electron';
+const { app, BrowserWindow, ipcMain, dialog, systemPreferences } = pkg;
+import dotenv from 'dotenv';
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import chokidar from 'chokidar';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const expressApp = express();
+const isDev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+
+
+
+dotenv.config({ path: path.join(__dirname, '.env') });
+
+// Express setup
+expressApp.use(
+  express.static(path.join(__dirname, 'dist'), {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.css')) {
+        res.setHeader('Content-Type', 'text/css');
+      }
+    },
+  })
+);
+
+expressApp.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self'"
+  );
+  next();
+});
+
+expressApp.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+});
+
+// Start Express server
+expressApp.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Electron setup
+let mainWindow;
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      preload: path.join(__dirname, '/src/scripts/functions.js'),
+    },
+  });
+
+  mainWindow.maximize();
+  mainWindow.loadURL(`http://localhost:${port}`);
+  mainWindow.loadFile(path.join(__dirname, '../../dist/index.html'));
+}
+
+if (isDev) {
+  const watcher = chokidar.watch(
+    [path.join(__dirname, 'pages'), path.join(__dirname, 'src/css')],
+    {
+      ignored: /(^|[\/\\])\../, // ignore dotfiles
+      persistent: true,
+    }
+  );
+
+  watcher.on('change', (path) => {
+    if (mainWindow) {
+      mainWindow.reload();
+    }
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const htmlPageNames = ['about'];
+const multipleHtmlPlugins = htmlPageNames.map((name) => {
+  return new HtmlWebpackPlugin({
+    template: `./src/pages/${name}.html`,
+    filename: `${name}.html`,
+    chunks: [`${name}`],
+  });
+});
+
+export default {
+  entry: {
+    index: ['./src/scripts/index.js', './src/css/index.scss'],
+    ...(fs.existsSync('./src/scripts/about.js') && {
+      about: ['./src/scripts/about.js', './src/css/about.scss'],
+    }),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                includePaths: [path.resolve(__dirname, 'src/css')],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.html',
+      chunks: ['index'],
+    }),
+  ].concat(multipleHtmlPlugins),
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.sass', '.scss', '.css'],
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+};
